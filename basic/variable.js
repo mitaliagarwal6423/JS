791976bae09cf9e2826b00c853bd6b7e64599cd8
mitaliagarwal6423 <59923438+mitaliagarwal6423@users.noreply.github.com>
2025-02-08 17:53:11 +0000

    variable

--- basic/variable.js
+++ basic/variable.js
@@ -2,6 +2,7 @@ const accountId = 12345
 let accountEmail = "mitaliagarwal@gmail.com"
 var accountPassword = "125678"
 accountCity = "Pune"
+let accountState // blank variable is read as undefined variable.
 /*
 var variable should not be used because of issue in block scope and functional scope. That's why we use let variable
 variable defined by any pre defined variable then also it is created
@@ -10,4 +11,4 @@ var and let can be changed later if needed.
 // accountId =89760
 // Cannot change constant variable in javascript
 console.log(accountId) // to print single varaible
-console.table([accountEmail, accountId,accountPassword,accountCity]) // to print all the variables in single go
+console.table([accountEmail, accountId,accountPassword,accountCity, accountState]) // to print all the variables in single go
