dd25a07e764ea4b7d244588c30bc0e963fcd4dac
mitaliagarwal6423 <59923438+mitaliagarwal6423@users.noreply.github.com>
2025-02-08 17:51:50 +0000

    variables

---
+++ basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "mitaliagarwal@gmail.com"
+var accountPassword = "125678"
+accountCity = "Pune"
+/*
+var variable should not be used because of issue in block scope and functional scope. That's why we use let variable
+variable defined by any pre defined variable then also it is created
+var and let can be changed later if needed.
+*/
+// accountId =89760
+// Cannot change constant variable in javascript
+console.log(accountId) // to print single varaible
+console.table([accountEmail, accountId,accountPassword,accountCity]) // to print all the variables in single go
